4a6ba91d15af5009a71ab89f47f828fca640be08
sasha walker <sashawalker31@gmail.com>
2019-07-27 21:23:12 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,10 @@
-def introduction(sasha)
+def introduction(name)
   puts="Hi, my name is #{name}"
 end
-def introduction_with_language(sasha, ruby)
+def introduction_with_language(name, language)
   puts="Hi, my name is #{name} and I am learning to program in #{language}."
 end
-def introduction_with_language_optional(sasha,ruby)
+def introduction_with_language_optional(name,language)
 puts="Hi, my name is #{name} and I am learning to program in #{language}"
 end
+introduction
